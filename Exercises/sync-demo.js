e15a992ded0d1ddb69f43a634b77d35405290727
Patrick Capuano <PCapuano978@west-mec.org>
2019-10-03 13:40:10 -0700

    Small Commit for Project Preparation.

--- Exercises/sync-demo.js
+++ Exercises/sync-demo.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+// https://nodejs.org/docs/latest-v8.x/api/fs.html#fs_fs_readdirsync_path_options
 var data = fs.readdirSync('../')
 console.log('data: ', data);
 console.log("This code is last")
