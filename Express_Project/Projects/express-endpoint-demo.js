c264f245cfda2e55ff1b1924885cb9dad3cff62a
Patrick Capuano <PCapuano978@west-mec.org>
2019-10-10 15:56:45 -0700

    Homework Time.
    The setup for the larger part of my project is now complete, so hopefully I actually work on it over the weekend.

--- Express_Project/Projects/express-endpoint-demo.js
+++ Express_Project/Projects/express-endpoint-demo.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 const port = 8080;
 app.use(express.static(__dirname));
 
-app.listen(port, () => console.log('Server listening on loacalhost:%s'), port);
+app.listen(port, () => console.log('Server listening on localhost:%s'), port);
 
 app.use('/', (req, res) => res.use('<h2>Hello from the server endpoint!</h2>'));
---
+++ Express_Project/Projects/server.js
@@ -0,0 +1,66 @@
+/**
+ * Following the example from glitch:
+ * (https://glitch.com/edit/#!/westmec-express-basics)
+ * 1. Require and use express
+ *      -  (https://expressjs.com/en/starter/hello-world.html)
+ * 2. Use **express.static** to make images, css, client-side js available
+ *      - (https://expressjs.com/en/starter/static-files.html)
+ * 3. set up a template engine
+ *      - (https://expressjs.com/en/guide/using-template-engines.html) 
+ *      - (https://expressjs.com/en/resources/template-engines.html)
+ *      - (https://expressjs.com/en/advanced/developing-template-engines.html)
+ * 4. set routes
+ *      - (https://expressjs.com/en/starter/basic-routing.html)
+ *      - (https://expressjs.com/en/guide/routing.html)
+ */
+
+const express = require('express')
+const app = express()
+const port = 8080
+
+// #2
+app.use(express.static("public"));
+
+
+/** TEMPLATE ENGINE
+* Express Handlebars is setup here
+* 2. Define the template engine (hbs aka handlebars)
+     - The default layout is the main.hbs file
+  3. Set the "view" / "template" engine
+    - [app.engine](https://expressjs.com/en/api.html#app.engine)
+    - [app.set](https://expressjs.com/en/api.html#app.set)
+*/
+const hbs = require('express-handlebars')({
+    defaultLayout: 'main',
+    extname: '.hbs'
+  });
+  app.engine('hbs', hbs);
+  app.set('view engine', 'hbs');
+
+const people = [
+    {
+        name: "Yehuda Katz",
+        age: 17
+    },
+    {
+        name: "Alan Johnson",
+        age: 17
+    },
+    {
+        name: "Charles Jolley",
+        age: 17
+    }
+  ];
+
+// app.get('/', (req, res) => res.send('Why, hello there!'))
+app.get("/", (request, response) => {
+    response.render('index', {
+      exampleVariable: "Hello, McMurdy",
+      people: people 
+    });
+  });
+
+
+app.listen(port, () => console.log('Example app listening on port${port}!'))
+// app.use('/', (req, res) => res.send('<h2>Hello from the server endpoint!</h2>'));
+
