a82da45aeb6045d6cb3d642bc2d2bc7a360305f5
Unknown <pcapuano978@west-mec.org>
2019-10-22 15:56:18 -0700

    Project Finale Cleanup Approaching.

--- Projects/public/poke.js
+++ Projects/public/poke.js
@@ -1,6 +1,63 @@
-const ID = "1"
+let ID = 1;
+
+var right = document.getElementById("rightButton").addEventListener("click", increase1);
+var left = document.getElementById("leftButton").addEventListener("click", decrease1);
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log(keyName);
+  
+    if (keyName === 'ArrowRight') {
+        increase1();
+    } else if(keyName === 'ArrowLeft') {
+        decrease1();
+    }
+
+});
 
 
+function increase1() {
+    ID++;
+    console.log(ID);    
+    fetch (`https://pokeapi.co/api/v2/pokemon/${ID}/`)
+.then((res) => res.json())
+.then( 
+    data => {
+        const {...pokemon} = data.sprites;
+        console.log(pokemon)
+        const selector = document.querySelector(".pokeIn");
+        selector.innerHTML = `<img src="${pokemon.front_default}" />`;
+        const selector2 = document.querySelector(".pokedex");
+        selector2.innerHTML = `${name}`;
+    }
+    
+);
+}
+
+function decrease1() {
+    ID--;
+    if (ID < 1) {
+        ID = 1;
+     } else if (ID == 0) {
+        ID = 1;
+    }
+
+    console.log(ID);    
+    fetch (`https://pokeapi.co/api/v2/pokemon/${ID}/`)
+.then((res) => res.json())
+.then( 
+    data => {
+        console.log(data);
+        name = data.name
+        const {...pokemon} = data.sprites;
+        console.log(pokemon)
+        const selector = document.querySelector(".pokeIn");
+        selector.innerHTML = `<img src="${pokemon.front_default}" />`;
+        const selector2 = document.querySelector(".pokedex");
+        selector2.innerHTML = `${name}`;
+    }
+);
+}
 
 fetch (`https://pokeapi.co/api/v2/pokemon/${ID}/`)
 .then((res) => res.json())
@@ -9,8 +66,8 @@ fetch (`https://pokeapi.co/api/v2/pokemon/${ID}/`)
         const {...pokemon} = data.sprites;
         console.log(pokemon)
         const selector = document.querySelector(".pokeIn");
-        selector.innerHTML = `<img src="${pokemon.front_default}" />`
+        selector.innerHTML = `<img src="${pokemon.front_default}" />`;
+        const selector2 = document.querySelector(".pokedex");
+        selector2.innerHTML = `${name}`;
     }
 );
-
-console.log(pokemon);
